57528c9b3b9f4316487d9de174477310ae71683f
jonandres45 <ansa1315@outlook.es>
2022-06-16 02:45:42 -0500

    corrigiendo

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
 import "./App.css";
+import Catalogo from './components/Catalogo';
+import Searchbar from './components/SearchBar';
+import OrderBy from './components/OrderBy';
+import Pagination from './components/Pagination';
 
 function App() {
   
   return (
     <React.StrictMode>
       <div style={{width:"99%", margin:'0 auto'}}>        
-      <h1>
-          Hola, mejor subi el proyecto a mi servidor :)
-          <br/>
-          pueden verlo desde este link <a href="https://mercado.andresjs.com">https://mercado.andresjs.com</a>
-        </h1>
+        <div className='grid justify-content-center'>
+          <div className="col-12 text-center">
+            <Searchbar/>
+            <OrderBy/>
+          </div>
+        </div>
+        <Pagination/>
+        <div className='grid justify-content-center'>
+          <Catalogo/>
+        </div>
+        <Pagination/>
       </div>
     </React.StrictMode>
   );
